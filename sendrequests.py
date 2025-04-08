a1c5c28c92f56cb72fec1058a20404ce7ab53ef3
Zyaojie <1750724954@qq.com>
2025-04-08 15:29:52 +0800

    封装get请求

---
+++ sendrequests.py
@@ -0,0 +1,17 @@
+import requests
+
+class SendRequests:
+    """
+    封装接口请求
+    """
+    def __init__(self):
+        pass
+
+    def get(self,url,data,header):
+        '''
+        封装get请求
+        :param url:
+        :param data:
+        :param header:
+        :return:
+        '''
