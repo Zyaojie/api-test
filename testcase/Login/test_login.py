3dd7ac74ca7bd1c8e637676440e41cefc6cba626
Zyaojie <1750724954@qq.com>
2025-05-08 17:22:21 +0800

    前后置

--- testcase/Login/test_login.py
+++ testcase/Login/test_login.py
@@ -4,26 +4,26 @@
 # @File : test_login
 # @Project : apitest
 import pytest
+from common.readyaml import get_testcase_yaml
 
+
+@pytest.fixture(scope='function',autouse=True,params=['1','2','3'])
+# scope:作用域，params:参数化，autouse:自动使用，ids:自定义ID，name:名称
+def fixture_test(request):
+    '''前后置处理'''
+    #print('--------------------接口测试开始--------------------')
+    #yield
+    #print('--------------------接口测试结束--------------------')
+    return request.param
 class TestLogin:
-    def setup_class(self):
-        '''在所有测试用例执行前只执行一次'''
-
-
-    def setup(self):
-        '''前置处理'''
-        print('在每个测试方法运行前都要执行我的代码')
 
     def test_case01(self):
-        print('我第二个执行')
+        print('用例1')
 
     #@pytest.mark.skip #跳过
     def test_case02(self):
-        print('我第三个执行')
-
-    def test_case03(self):
-        print('我第一个执行')
-
-    def teardown(self):
-        '''后置处理'''
-        print('在每个测试方法运行后都要执行我的代码')
+        print('用例2')
+    @pytest.mark.maoyan
+    def test_case03(self,fixture_test):
+        print('用例3')
+        print('获取到的参数值为：',fixture_test)
